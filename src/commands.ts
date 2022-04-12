f2d42bdcb0fa3f99a6e9688b7e6890ad06267f57
Juan González <juan.gonzalez@femtasy.com>
2022-04-12 16:56:22 +0200

    Lunch command

---
+++ src/commands.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { WebClient } from "@slack/web-api";
+
+const webClient = new WebClient(process.env.SLACK_TOKEN);
+
+interface LunchParams {
+  text: string;
+  emoji: string;
+  expirationInMs: number;
+}
+
+export const setLunch = async ({
+  text,
+  emoji,
+  expirationInMs,
+}: LunchParams): Promise<void> => {
+  try {
+    const { profile } = await webClient.users.profile.set({
+      profile: JSON.stringify({
+        status_text: text,
+        status_emoji: emoji,
+        status_expiration: (
+          new Date().getTime() / 1000 +
+          expirationInMs
+        ).toString(),
+      }),
+    });
+
+    if (profile) {
+      const { status_text, status_emoji, status_expiration } = profile;
+
+      console.log(
+        "Lunchtime status set to:",
+        status_text,
+        status_emoji,
+        status_expiration && new Date(status_expiration * 1000),
+      );
+    } else {
+      console.warn("No profile in response!");
+    }
+
+    process.exit(0);
+  } catch (error) {
+    console.error("Request failed:", error);
+    process.exit(1);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-import "source-map-support/register";
+import "dotenv/config";
 
-console.log("Hello world!");
+import yargs from "yargs";
+import { setLunch } from "./commands";
+
+const lunchMessage = process.env.LUNCH_MESSAGE;
+const lunchEmoji = process.env.LUNCH_SLACK_EMOJI;
+const lunchExpirationInSeconds = process.env.LUNCH_EXPIRATION_SECONDS;
+
+if (!lunchMessage)
+  throw new Error("LUNCH_MESSAGE is not set, check your .env file");
+if (!lunchEmoji)
+  throw new Error("LUNCH_SLACK_EMOJI is not set, check your .env file");
+if (!lunchExpirationInSeconds)
+  throw new Error("LUNCH_EXPIRATION_SECONDS is not set, check your .env file");
+
+const lunchExpirationInMinutes = parseInt(lunchExpirationInSeconds, 10) / 60;
+
+yargs
+  .scriptName("slackStatus")
+  .command(
+    "lunch",
+    `Sets status to '(${lunchEmoji}) ${lunchMessage}' expiring in '${lunchExpirationInMinutes}' minutes`,
+    () =>
+      setLunch({
+        text: lunchMessage,
+        emoji: lunchEmoji,
+        expirationInMs: parseInt(lunchExpirationInSeconds),
+      }),
+  )
+  .help().argv;
